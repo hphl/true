68cd8c49653e65df535fdfe861f677f3fda4a3c3
“hphl” <hp_hamel@hotmail.com>
2018-01-26 09:52:21 -0400

    fixed the styles from both pages. Also fixed the next page from the user's repositories.

--- javascript.js
+++ javascript.js
@@ -1,66 +1,82 @@
+
 $( document ).ready(function() {
-  var curl = 'https://api.github.com/users?since=0';
+  getUsers(); //creates the users from github
   var btn_load = createNode("button","btn btn-primary btn-load-users");
-  btn_load.setAttribute("data-link",curl);
   btn_load.textContent = "Load More Users";
   append(document.getElementById("pagination"), btn_load);
-  getUsers(curl);
   $(".btn-load-users").click(function(){
     getUsers($(this).attr("data-link"));
   });
 });
+/*function that gets the users from github and creates the layout*/
 function getUsers(url){
+  if(typeof url === 'undefined'){ //if there is no url in the parameter, it will set a default url
+    url = 'https://api.github.com/users?since=0';
+  }
   fetch(url,{
-      method: 'GET',
       headers: {
-          'Accept': 'application/json',
+          'Accept': 'application/json'
       }
   })
   .then(function(response) {
-    var pagination_links = response.headers.get('Link').split(',');
-    var next_page = pagination_links[0].split(';');
-    $("#pagination .btn-load-users").attr("data-link",next_page[0].replace(/<|>/g,''));
+    var pagination_links = response.headers.get('Link').split(','),
+        next_page = pagination_links[0].split(';');
+    $(".btn-load-users").attr("data-link",next_page[0].replace(/<|>/g,''));
     return response.json(); // Transform the data into json
   })
   .then(function(data) {
     //console.log(data);
-    data.forEach(createUserNode);
+    data.forEach(createUserNode);//creates the layout of each user
   })
   .catch(function(error) {
     // If there is any error it will catch here
     console.log(error);
   });
 }
+/*creates a new html element*/
 function createNode(element, classes){
-  var new_element = document.createElement(element);
-  if(typeof classes !== 'undefined')
+  var new_element = document.createElement(element); //creates the element
+  if(typeof classes !== 'undefined') //checks if there are classes in the parameters to assign in the element
     new_element.className = classes;
   return new_element;
 }
-function append(parent, el) {
-    return parent.appendChild(el);
+/*appends an element to another element*/
+function append(parent, element) {
+    return parent.appendChild(element);
 }
+/*creates the layout of a user*/
 function createUserNode(user){
   var user_img = createNode("img", "img-responsive"),
       user_github = createNode("a", "btn btn-primary"),
-      user_repositories = createNode("a", "btn btn-default pull-right"),
-      user_name = createNode("h3"),
-      column = createNode("div", "col-sm-3"),
+      user_repositories = createNode("a", "btn btn-default"),
+      user_name = createNode("h3","text-center flex-text"),
+      column = createNode("div", "col-xs-6 col-sm-4 col-md-3"),
       user_box = document.getElementById('users-box'),
       thumbnail_div = createNode("div","thumbnail"),
-      caption_div = createNode("div","caption");
+      caption_div = createNode("div","caption"),
+      list_inline = createNode("ul","list-inline text-center"),
+      li_elem_one = createNode("li"),
+      li_elem_two = createNode("li");
+
       user_img.src = user.avatar_url;
       user_github.setAttribute("href", user.html_url);
       user_github.setAttribute("target","_blank");
       user_github.textContent = "Git Account";
-      user_repositories.setAttribute("href",document.URL.substr(0,document.URL.lastIndexOf('/')) + '/repository.html?' + "user=" + user.login);
+      user_repositories.setAttribute("href",document.URL.substr(0,document.URL.lastIndexOf('/')) + '/repository.html?' + 'user=' + user.login + '&page=1&per_page=8');
       user_repositories.textContent = "Repositories";
       user_name.textContent = user.login;
+
       append(user_box,column);
       append(column,thumbnail_div);
       append(thumbnail_div,user_img);
       append(thumbnail_div,caption_div);
       append(caption_div,user_name);
-      append(caption_div,user_github);
-      append(caption_div,user_repositories);
+      append(caption_div,list_inline);
+      append(list_inline,li_elem_one);
+      append(li_elem_one,user_github);
+      append(list_inline,li_elem_two);
+      append(li_elem_two,user_repositories);
+}
+function basicStructure(){
+  
 }
--- javascript_repository.js
+++ javascript_repository.js
@@ -1,25 +1,28 @@
+
 $( document ).ready(function() {
-  var user_name = getUrlParameter('user');
-  var curl = 'https://api.github.com/users/' + user_name +'/repos?page=1&per_page=8';
   var btn_next = createNode("a","btn btn-primary btn-next");
-  btn_next.setAttribute("href",curl);
   btn_next.textContent = "Next Page";
   append(document.getElementById("pagination"), btn_next);
-  getRepositories(curl);
+  getRepositories();
 });
-function getRepositories(url){
+/*creates the repositories to display*/
+function getRepositories(){
+  var user_name = getUrlParameter('user'),
+      current_page = getUrlParameter("page"),
+      per_page = getUrlParameter("per_page"),
+      url = 'https://api.github.com/users/' + user_name +'/repos?page=' + current_page + '&per_page=' + per_page;
   fetch(url,{
-      method: 'GET',
       headers: {
-          'Accept': 'application/json',
+          'Accept': 'application/json'
       }
   })
   .then(function(response) {
-    console.log(response);
     var pagination_links = response.headers.get('Link').split(',');
     var next_page = pagination_links[0].split(';');
-    console.log(next_page);
-    $("#pagination .btn-next").attr("href",next_page[0].replace(/<|>/g,''));
+    next_page = next_page[0].replace(/<|>/g,'');
+    var next_page_num = getUrlParameter("page",next_page);
+    var base_url = document.URL.substring(0,document.URL.indexOf('?')+1);
+    $(".btn-next").attr("href",base_url +'&user=' + user_name + '&page=' + next_page_num + '&per_page=' +  per_page);
     return response.json(); // Transform the data into json
   })
   .then(function(data) {
@@ -40,45 +43,52 @@ function createNode(element, classes){
 function append(parent, el) {
     return parent.appendChild(el);
 }
-function getUrlParameter(param) {
-    var pageURL = decodeURIComponent(window.location.search.substring(1)),
-        URLVariables = pageURL.split('&'),
-        parameterName;
-
-    for (var i = 0; i < URLVariables.length; i++) {
-        parameterName = URLVariables[i].split('=');
-
-        if (parameterName[0] === param) {
-            return parameterName[1] === undefined ? true : parameterName[1];
+function getUrlParameter(param, page_url) {
+    if(typeof page_url === 'undefined'){
+      page_url = decodeURIComponent(window.location.search.substring(1));
+    }else{
+      page_url = page_url.substring(page_url.indexOf('?') + 1);
+    }
+    var url_variables = page_url.split('&'),
+        parameter_name;
+    for (var i = 0; i < url_variables.length; i++) {
+        parameter_name = url_variables[i].split('=');
+        if (parameter_name[0] === param) {
+            return parameter_name[1] === undefined ? true : parameter_name[1];
         }
     }
 }
 function createRepoNode(repo){
-  var repo_github = createNode("a", "btn btn-primary"),
-      repo_name = createNode("p"),
-      repo_description = createNode("p"),
-      repo_forks = createNode("p"),
-      repo_issues = createNode("p");
-      repo_open_issues = createNode("p");
-      column = createNode("div", "col-sm-3"),
+  var repo_github = createNode("a", "btn btn-primary center-block"),
+      repo_name = createNode("h3","text-center ellipsis"),
+      repo_description = createNode("p","flex-text"),
+      repo_forks = createNode("li","list-group-item"),
+      repo_open_issues = createNode("li","list-group-item");
+      column = createNode("div", "col-xs-6 col-sm-4 col-md-3"),
       repo_box = document.getElementById('repositories-box'),
       thumbnail_div = createNode("div","thumbnail"),
-      caption_div = createNode("div","caption");
+      caption_div = createNode("div","caption"),
+      badge_forks = createNode("span","badge"),
+      badge_open_issues = createNode("span","badge"),
+      list_group = createNode("ul", "list-group");
       repo_github.setAttribute("href", repo.html_url);
       repo_github.setAttribute("target","_blank");
       repo_github.textContent = "Github";
       repo_name.textContent = repo.name;
       repo_description.textContent = repo.description;
-      repo_forks.textContent = repo.forks_count;
-      repo_issues.textContent = repo.open_issues;
-      repo_open_issues.textContent = repo.open_issues;
+      repo_forks.textContent = "Forks ";
+      badge_forks.textContent = repo.forks_count;
+      repo_open_issues.textContent = "Open Issues ";
+      badge_open_issues.textContent = repo.open_issues;
       append(repo_box,column);
       append(column,thumbnail_div);
       append(thumbnail_div,caption_div);
-      append(caption_div,repo_github);
       append(caption_div,repo_name);
       append(caption_div,repo_description);
-      append(caption_div,repo_forks);
-      append(caption_div,repo_issues);
-      append(caption_div,repo_open_issues);
+      append(caption_div,list_group);
+      append(list_group,repo_open_issues);
+      append(repo_open_issues,badge_open_issues);
+      append(list_group,repo_forks);
+      append(repo_forks,badge_forks);
+      append(caption_div,repo_github);
 }
